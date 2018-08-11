2f42b61e2bde2cb0abe2fe0fa7ab910485e36936
Mai Chee Vang <maicheevang@Mais-MacBook-Pro.local>
2018-08-11 16:40:33 -0500

    Mongo connected

--- server/server.js
+++ server/server.js
@@ -4,6 +4,19 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
+//mongoose require
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://localhost:27017/tasksToDo';
+
+//attempt to connect to mongo
+mongoose.connect(mongoURI, {useNewUrlParser: true});
+mongoose.connection.on('open', () => {
+    console.log('connected to Mongo');
+});
+mongoose.connection.on('error', () => {
+    console.log('error connecting to Mongo');
+});
+
 //uses
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
